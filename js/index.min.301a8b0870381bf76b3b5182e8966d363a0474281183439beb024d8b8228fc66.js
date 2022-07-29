c7754013e108f9448a6d573d8cf681b97c83a485
Adam Kostarelas <github@adam.kostarelas.com>
2022-07-29 17:06:03 +1000

    fix: purge

--- js/index.min.301a8b0870381bf76b3b5182e8966d363a0474281183439beb024d8b8228fc66.js
+++
@@ -1,1 +0,0 @@
-function toggleMobileNavState(){const e=document.querySelector("body");e.classList.toggle("nav--active")}function initBurger(){const e=document.querySelector(".burger");e.addEventListener("click",toggleMobileNavState)}initBurger()
